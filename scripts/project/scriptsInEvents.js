299c8779225fdeba50b863a5aebdecfbd8f715bc
gamepush0001 <149809262+gamepush0001@users.noreply.github.com>
2023-11-03 18:19:59 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async Game_Event2_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#a2b0bf'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
